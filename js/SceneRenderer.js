b713cf2822538e3a4aff1f2432af4159a2ded133
Aoife Fogarty <afogarty1000@yahoo.com>
2017-08-19 21:11:10 +0200

    control hero placement on bg image

--- js/SceneRenderer.js
+++ js/SceneRenderer.js
@@ -20,6 +20,8 @@ function SceneRenderer(context, model) {
 
     // Background image
     this._backgroundImage = new Image();
+    this._backgroundImage.onload = function() {
+    }
     this._backgroundImage.src = "images/game_backdrop.png";
     
     // hero image
@@ -35,8 +37,8 @@ _p = SceneRenderer.prototype;
 _p.repaint = function() {
     this._ctx.save();
     this._ctx.translate(this._x, this._y);
-    this._drawBackground();
-    this._drawHero();
+    bgImageSize = this._drawBackground();
+    this._drawHero(bgImageSize.width*0.2, bgImageSize.height*0.8); //TODO hero class with coordinqtes
     //this._drawSprites();
     this._ctx.restore();
 };
@@ -44,9 +46,10 @@ _p.repaint = function() {
 _p._drawBackground = function() {
     var ctx = this._ctx;
     ctx.drawImage(this._backgroundImage, 0, 0);  // don't hardcode 00 here TODO, check if loaded
+    return {width: this._backgroundImage.width, height: this._backgroundImage.height};
 };
 
-_p._drawHero = function() {
+_p._drawHero = function(x, y) {
     var ctx = this._ctx;
-    ctx.drawImage(this._heroImage, 0, 0);  // don't hardcode 00 here TODO, check if loaded
+    ctx.drawImage(this._heroImage, x, y);  // TODO, check if loaded
 };
