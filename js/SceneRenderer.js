cee70bd9233777d5e13ae17429036bc99b3360a7
Aoife Fogarty <afogarty1000@yahoo.com>
2017-08-19 23:50:24 +0200

    add food sprites

--- js/SceneRenderer.js
+++ js/SceneRenderer.js
@@ -27,6 +27,27 @@ function SceneRenderer(context, model) {
     // hero image
     this._heroImage = new Image();
     this._heroImage.src = "images/hero.png";
+
+    //food sprite sheet
+    this._foodImage = new Image();
+    this._foodImage.src = "images/food.png";
+
+    this._foodTypes = ['GOOD','BAD','GOOD','GOOD','BAD','GOOD']; //TODO make dict
+    this._foodSpriteCropX = {'APPLE': 0, 'CHEESE': 40, 'LEMON': 80, 'CARROT': 120, 'PIZZA': 160, 'GRAPES': 200};
+    this._foodSpriteWidth = 40;
+    this._foodSpriteHeight = 40;
+
+    this._foodSprites = [];
+    var sprite1 = new Sprite()
+    sprite1.x = 100;
+    sprite1.y = 100;
+    this._foodSprites.push(sprite1);
+}
+
+function Sprite() {
+    this.type = 'APPLE';
+    this.x = 0;
+    this.y = 0;
 }
 
 _p = SceneRenderer.prototype;
@@ -39,7 +60,7 @@ _p.repaint = function() {
     this._ctx.translate(this._x, this._y);
     bgImageSize = this._drawBackground();
     this._drawHero(bgImageSize.width*0.2, bgImageSize.height*0.8); //TODO hero class with coordinqtes
-    //this._drawSprites();
+    this._drawFood(this._foodSprites);
     this._ctx.restore();
 };
 
@@ -52,4 +73,17 @@ _p._drawBackground = function() {
 _p._drawHero = function(x, y) {
     var ctx = this._ctx;
     ctx.drawImage(this._heroImage, x, y);  // TODO, check if loaded
+};
+
+_p._drawFood = function(foodSprites) {
+    var ctx = this._ctx;
+    var swidth = this._foodSpriteWidth; // width of cropped image
+    var sheight = this._foodSpriteHeight; // height of cropped image
+    for (i = 0; i < foodSprites.length; i++) {
+        var x = foodSprites[i].x;
+        var y = foodSprites[i].y;
+        var sx = this._foodSpriteCropX[foodSprites[i].type]; // start crop
+        var sy = 0; // start crop
+        ctx.drawImage(this._foodImage, sx, sy, swidth, sheight, x, y, swidth, sheight);  // TODO, check if loaded
+    }
 };
