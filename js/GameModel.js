d74ed9b274bb9d60080c5843a18d84d70cfc8136
Aoife Fogarty <afogarty1000@yahoo.com>
2017-08-20 17:34:54 +0200

    animate falling food sprite

--- js/GameModel.js
+++ js/GameModel.js
@@ -1,15 +1,29 @@
 /**
  * The GameModel is the class that is responsible for
- * storing the positions of the players,
- * checking the win and lose conditions,
+ * storing the positions of the hero and food sprites,
+ * checking the win condition,
  * and reporting the score.
  */
-function GameModel(cols, rows) {
+function GameModel() {
 	
-	this.reset();
+    this._foodSprites = [];
+    var sprite1 = new Sprite()
+    this._foodSprites.push(sprite1);
+}
+
+function Sprite() {
+    types = ['APPLE', 'CHEESE', 'LEMON', 'CARROT', 'PIZZA', 'GRAPES']
+    this.type = types[randomIntFromInterval(0, 5)];
+    this.x = randomIntFromInterval(0, 300);
+    this.y = 0;
+    this.speed = 10;  // pixels per frame
 }
 
 _p = GameModel.prototype;
 
 _p.reset = function() {
+    console.log('reset');
+    for (i = 0; i < this._foodSprites.length; i++) {
+        this._foodSprites[i].y += this._foodSprites[i].speed;
+    }
 };
