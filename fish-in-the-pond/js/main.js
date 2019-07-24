248d8d61a042cd34157f5143646f144b77b88a61
Aoife Fogarty <afogarty1000@yahoo.com>
2019-07-24 21:41:16 +0200

    [fish-pond] working game

--- fish-in-the-pond/js/main.js
+++ fish-in-the-pond/js/main.js
@@ -1,7 +1,9 @@
 // types of playing cards in the deck
-var cardTypes = ["circle", "cross", "square", "triangle"];
+var faceCardTypes = ["circle", "cross", "square", "triangle"];
+var cardTypes = faceCardTypes.concat(["back"]);
 var cardWidth = 70; // in pixels
 var cardHeight = 100;
+var cardImages = {}; // contains loaded image objects for each card type
 
 // grid used to lay out playing cards
 var nColumns = 10;
@@ -9,13 +11,13 @@ var nRows = 5;
 var nCards = nColumns * nRows; // TODO has to be even number
 var cardsGrid = [];
 
-var cardImages = {};
+// list of max 2 cards clicked by the player
+cardsClicked = [];
 
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = cardWidth * nColumns;
 canvas.height = cardHeight * nRows;
-console.log(canvas.width);
 
 function init() {
 
@@ -42,13 +44,16 @@ function init() {
     }
   }
 
-  // TODO event listener for clicks
-
-  // start button
-  var button = document.getElementById('startButton');
-  button.addEventListener('click', function(e) {
-    startGame();
+  // event listener for clicks on cards
+  canvas.addEventListener('click', function(e) {
+    reactToClick(e);
   }, false);
+
+  // // start button
+  // var button = document.getElementById('startButton');
+  // button.addEventListener('click', function(e) {
+  //   startGame();
+  // }, false);
 
   startGame();
 
@@ -59,15 +64,12 @@ function startGame() {
   nPairsMatched = 0;
 
   // create shuffled deck containing pairs of cards
-  deck = createDeck(nCards, cardTypes);
+  deck = createDeck(nCards, faceCardTypes);
 
   // place cards on grid
   for (var i = 0; i < nRows; i++) {
-    //console.log(i);
     cardsGrid.push([]);
     for (var j = 0; j < nColumns; j++) {
-      //console.log(j);
-      //console.log(i*nColumns + j);
       cardsGrid[i].push(deck[i*nColumns + j]);
     }
   }
@@ -77,9 +79,8 @@ function startGame() {
 }
 
 var render = function () {
-	//if (bgReady) {
-	//	ctx.drawImage(bgImage, 0, 0);
-	//}
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawCards();
 
@@ -87,15 +88,15 @@ var render = function () {
 
 
 // create shuffled deck containing pairs of cards
-function createDeck(nCards, cardTypes) {
+function createDeck(nCards, faceCardTypes) {
   var deck = [];
-  var nCardTypes = cardTypes.length;
+  var nCardTypes = faceCardTypes.length;
   for (var i = 0; i < nCards / 2; i++) {
     // randomly choose a card type
     var irandom = Math.floor(Math.random() * nCardTypes);
     // put two cards of that type in the deck
-    deck.push(cardTypes[irandom]);
-    deck.push(cardTypes[irandom]);
+    deck.push(faceCardTypes[irandom]);
+    deck.push(faceCardTypes[irandom]);
   }
   deck = shuffle(deck);
   return deck;
@@ -104,30 +105,75 @@ function createDeck(nCards, cardTypes) {
 
 // place cards on canvas
 function drawCards() {
-  console.log('here1');
+
   for (var i = 0; i < nRows; i++) {
     for (var j = 0; j < nColumns; j++) {
+
       // dont draw cards which have already been matched
-      if (cardsGrid[i][j] == 'vanished') {
+      if (cardsGrid[i][j] == 'removed') {
         continue;
       }
-      //
-      image = cardImages[cardsGrid[i][j]];
-      console.log(i);
-      console.log(j);
-      console.log(image);
+
+      // check if this card has been clicked by the user
+      var card = [i, j];
+      card = JSON.stringify(card);
+      var present = JSON.stringify(cardsClicked).indexOf(card);
+      if (present != -1) {
+        // cards chosen by user are facing up
+        image = cardImages[cardsGrid[i][j]];
+      } else {
+        // all other cards are facing down
+        image = cardImages["back"];
+      }
+
       ctx.drawImage(image, j * cardWidth, i * cardHeight, cardWidth, cardHeight);
     }
   }
 }
 
-// react to clicks
-function reactToClicks(card1, card2) {
-  if (cardsGrid[card1[0]][card1[1]] == cardsGrid[card2[0]][card2[1]]) {
-    cardsGrid[card1[0]][card1[1]] == 'vanished';
-    cardsGrid[card2[0]][card2[1]] == 'vanished';
+function reactToClick(e) {
+  // find which card was clicked
+  var rect = canvas.getBoundingClientRect()
+  var x = e.clientX - rect.left
+  var y = e.clientY - rect.top
+  var i = Math.floor(y / cardHeight);
+  var j = Math.floor(x / cardWidth);
+  cardsClicked.push([i,j]);
+
+  // only have a maximum of 2 cards turned over at one time
+  if (cardsClicked.length > 2) {
+    cardsClicked.shift();
   }
+
+  render();
+
+  if (cardsClicked.length == 2) {
+    checkGuess();
+  }
+
 }
+
+
+// check if the two cards are the same
+function checkGuess() {
+
+  var card1 = cardsClicked[0];
+  var card2 = cardsClicked[1];
+  if (cardsGrid[card1[0]][card1[1]] == cardsGrid[card2[0]][card2[1]]) {
+    cardsGrid[card1[0]][card1[1]] = 'removed';
+    cardsGrid[card2[0]][card2[1]] = 'removed';
+    nPairsMatched++;
+    window.alert('Well done!');
+  } else {
+    window.alert('Wrong!');
+  }
+
+  // turn the cards back over
+  cardsClicked = [];
+
+  render();
+}
+
 
 function shuffle(array) {
   for (var i = array.length - 1; i > 0; i--) {
